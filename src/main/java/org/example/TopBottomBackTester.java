b1b962455527cc07613eb701ecc63e09ced0e16c
gangadhar <gangadhar.balikai@gmail.com>
2023-10-17 21:16:19 +0800

    added proper logging.

--- src/main/java/org/example/TopBottomBackTester.java
+++ src/main/java/org/example/TopBottomBackTester.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.text.MessageFormat;
 import java.time.LocalDate;
 import java.time.Month;
 
@@ -14,8 +15,6 @@ class TopBottomBackTester implements BackTester {
 
     @Override
     public void test() {
-
-
         InstrumentType instrumentType = InstrumentType.EQUITY;
         Position position = Position.LONG;
         double stopLose = 18034;
@@ -26,7 +25,6 @@ class TopBottomBackTester implements BackTester {
         boolean isAdjusted = false;
         LocalDate tradeDate = LocalDate.of(2016, Month.DECEMBER, 29);
 
-
         for (int i = 2; i < historicPrice.length; i++) {
             double dayBeforeYesterdayClose = historicPrice[i - 2].close();
             double yesterdaysClose = historicPrice[i - 1].close();
@@ -35,8 +33,15 @@ class TopBottomBackTester implements BackTester {
             LocalDate today = historicPrice[i].date();
 
             if (!isAdjusted) {
+                var previousStop = stopLose;
                 stopLose = adjustStopLose(position, stopLose, tradePrice, todayClose);
                 isAdjusted = true;
+
+                if (previousStop != stopLose) {
+                    System.out.println(MessageFormat.format("{0} adjusting top lose for in the money price from {1} to {2}",
+                            today, previousStop, stopLose));
+                }
+
             }
             //up trend
             if (yesterdaysClose < dayBeforeYesterdayClose && yesterdaysClose < todayClose) {
@@ -45,24 +50,33 @@ class TopBottomBackTester implements BackTester {
                 if (Double.isNaN(top1)) {
                     top1 = yesterdaysClose;
                 } else {
+                    System.out.println(MessageFormat.format("{0} updating top from {1} to {2}, previous top1 at {3} ", today, top2, yesterdaysClose, top1));
                     top2 = yesterdaysClose;
                 }
             }
             //double top
             if (top2 > top1) {
                 if (Position.LONG == position) {
+                    System.out.println(MessageFormat.format("{0} moving stop lose on {1} position from : {2} to {3} (Top2 : {4} > Top1 :{5})",
+                            today, position, stopLose, low, top2, top1));
                     stopLose = low;
                 }
             }
             //double bottom
             else if (top2 < top1) {
                 if (Position.SHORT == position) {
+                    System.out.println(MessageFormat.format("{0} moving stop lose on {1} position from : {2} to {3} (Top2 : {3} < Top1 :{4})",
+                            today, position, stopLose, top2, top1));
                     stopLose = top2;
                 }
             }
             //
             //Check stop loose
             if (Position.LONG == position && stopLose >= todayClose) {
+
+                System.out.println(MessageFormat.format("{0} closing position {1} entry {2} exit {3}"
+                        , tradeDate, position, tradePrice, stopLose));
+
                 tradeRepo.insert(tradeDate, Position.LONG, tradePrice, stopLose);
                 tradeDate = today;
                 tradePrice = stopLose;
@@ -71,8 +85,12 @@ class TopBottomBackTester implements BackTester {
                 top1 = top2;
                 top2 = Double.NaN;
                 isAdjusted = false;
+                System.out.println(MessageFormat.format("{0} entered into new position {1} entry {2} exit {3}"
+                        , today, Position.SHORT, tradePrice, stopLose));
 
             } else if (Position.SHORT == position && stopLose <= todayClose) {
+                System.out.println(MessageFormat.format("{0} closing position {1} entry {2} exit {3}"
+                        , tradeDate, position, tradePrice, stopLose));
                 tradeRepo.insert(tradeDate, Position.SHORT, tradePrice, stopLose);
                 tradeDate = today;
                 tradePrice = stopLose;
@@ -81,6 +99,8 @@ class TopBottomBackTester implements BackTester {
                 top1 = top2;
                 top2 = Double.NaN;
                 isAdjusted = false;
+                System.out.println(MessageFormat.format("{0} entered into new position {1} position entry {2} exit {3}"
+                        , today, position, tradePrice, stopLose));
             }
         }
     }
